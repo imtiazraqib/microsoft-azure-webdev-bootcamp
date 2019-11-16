b62f5a8efdf944c2eb9417dceccc96b1330a8acd
imtiazraqib <imtiazud@ualberta.ca>
2019-11-16 15:20:25 -0700

    added reset logic

--- code/guess-the-number/scripts/app.js
+++ code/guess-the-number/scripts/app.js
@@ -17,15 +17,39 @@
         - change the classes of the elements between "win" and "stuff"
 */
 
+const generateRandomNumberWithinRange = function(min, max) {
+   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const submitNumber = function (event) {
     event.preventDefault();     // prevents the screen from refreshing and only listens
                                 // if submit is hit again.
 
-    const userInput = document.getElementById('numberTextInput').value;
-    console.log(userInput);
+    const userInput = Number(document.getElementById('numberTextInput').value);
+    // if the user does not enter a number, the number conversion of a non-number
+    // returns NaN - Not a Number
+
+    if (Number.isNaN(userInput)) {
+        window.alert("Please enter a number only!");
+        return;
+    }
+}
+
+const restartGame = function (event) {
+    const min = 1;
+    const max = 20;
+
+    // window is basically creating a global variable
+    window.randomNumber = generateRandomNumberWithinRange(min, max);
+
+    // reset the counter
+    window.attempts = 0;
+        
 }
 
 document.querySelector("#taskForm").addEventListener("submit", submitNumber);
+document.querySelector("#taskForm").addEventListener("reset", restartGame)
+restartGame();
 
 
 
