08dadc3d6973fe6df0877ae2e396a3212bf2c685
imtiazraqib <imtiazud@ualberta.ca>
2019-11-18 23:27:31 -0700

    able to retrieve user input

---
+++ code/to-do-list/scripts/main.js
@@ -0,0 +1,9 @@
+
+// retrieving the user inputted task
+const submitTask = function () {
+    
+    const userInput = document.getElementById("taskTextInput").value;
+}
+
+
+document.querySelector("#taskForm").addEventListener("submit", submitTask);
