f6a39c3872536f469edf41e0484a530054b2757e
imtiazraqib <imtiazud@ualberta.ca>
2019-11-23 09:22:49 -0700

    Day 2 initialized

--- code/to-do-list/scripts/main.js
+++ code/to-do-list/scripts/main.js
@@ -1,9 +1,21 @@
+let taskArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : []
 
 // retrieving the user inputted task
-const submitTask = function () {
-    
+const submitTask = function (event) {
+    event.preventDefault();
+
     const userInput = document.getElementById("taskTextInput").value;
+    if (!taskTextInput.value) {
+        window.alert('Please enter a task');
+        return;
+    }
+
+    taskArray.push(userInput);
+    console.log(taskArray);
 }
 
+const updateUIwithTask = function () {
+
+}
 
 document.querySelector("#taskForm").addEventListener("submit", submitTask);
