7fb3620c0eef193eefda2ae829278f4d674c2f6f
imtiazraqib <imtiazud@ualberta.ca>
2019-11-23 09:32:24 -0700

    Creating objects

---
+++ code/basic-lessons/basic-lessons-day2.js
@@ -0,0 +1,15 @@
+// To add strings, we can just put a comma instead of the plus sign
+let name = "Imtiaz"
+let secondName = "Vanessa"
+console.log("My name is", name, secondName);
+
+// Creating objects
+
+const myObject = {};
+myObject.name = "Imtiaz";                       // Creating a key "name" with its value
+console.log(myObject);
+
+
+const anotherObject = {
+    name: "Imtiaz",                             // Creating a key "name" with its value explicitly
+}
